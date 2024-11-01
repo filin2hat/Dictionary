2fb1c96d5292b4d365295aa175dc1a5ca39d67b6
Dmitriy Biryulin <biryulin.develop@gmail.com>
2024-11-01 10:35:28 +0300

    add DictionaryEntry "key" "value"

---
+++ src/entry/DictionaryEntry.java
@@ -0,0 +1,24 @@
+package entry;
+
+public class DictionaryEntry {
+    private String key;
+    private String value;
+
+    public DictionaryEntry(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return key + " - " + value;
+    }
+}
